fa224f60720cfee1de3913a241029a20946f47d8
Satit Rianpit <rianpit@gmail.com>
2016-12-08 11:51:51 +0700

    add basic service

--- src/providers/service.ts
+++ src/providers/service.ts
@@ -1,18 +1,29 @@
-import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Injectable, Inject } from '@angular/core';
+import { Http, Headers, RequestOptions } from '@angular/http';
 import 'rxjs/add/operator/map';
 
-/*
-  Generated class for the Service provider.
-
-  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
-  for more info on providers and Angular 2 DI.
-*/
 @Injectable()
 export class Service {
 
-  constructor(public http: Http) {
+  constructor(public http: Http, @Inject('API_URL') private url: string) {
     console.log('Hello Service Provider');
+  }
+
+  getServices(vstdate: string) {
+    return new Promise((resolve, reject) => {
+      let headers = new Headers({ 'Content-Type': 'application/json' });
+      let options = new RequestOptions({ headers: headers });
+      let body = { vstdate: vstdate };
+
+      this.http.post(`${this.url}/users`, body, options)
+      // this.http.get(this.url + '/users')
+        .map(res => res.json())
+        .subscribe(data => {
+          resolve(data)
+        }, err => {
+          reject(err)
+        });
+    });
   }
 
 }
