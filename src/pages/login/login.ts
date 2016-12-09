91e64ce0051ca86b5e5bad7a7e3c0a9d1c4eeddf
Satit Rianpit <rianpit@gmail.com>
2016-12-09 13:45:43 +0700

    add loading login

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, LoadingController, AlertController } from 'ionic-angular';
 import { Service } from '../../providers/service';
 import { IHttpResult } from '../../models';
 import { TabsPage } from '../tabs/tabs';
@@ -12,20 +12,49 @@ export class LoginPage {
   username: string;
   password: string;
 
-  constructor(public navCtrl: NavController, private serviceProvider: Service) {}
+  constructor(
+    public navCtrl: NavController,
+    private serviceProvider: Service,
+    private loadingCtrl: LoadingController,
+    private alertCtrl: AlertController
+  ) { }
 
   login() {
+    let loading = this.loadingCtrl.create({
+      content: 'กำลังเข้าสู่ระบบ...'
+    });
+
+    loading.present();
+
     this.serviceProvider.login(this.username, this.password)
       .then((data: IHttpResult) => {
+        loading.dismiss();
         if (data.ok) {
           let token = data.token;
           localStorage.setItem('token', token);
+          let alert = this.alertCtrl.create({
+            title: 'Welcome!',
+            subTitle: 'สวัสดี, ' + data.fullname,
+            buttons: ['OK']
+          });
+          alert.present();
           this.navCtrl.setRoot(TabsPage);
         } else {
-          alert(data.error);
+          let alert = this.alertCtrl.create({
+            title: 'Error!',
+            subTitle: 'เกิดข้อผิดพลาด, ' + data.error,
+            buttons: ['OK']
+          });
+          alert.present();
         }
       }, (err) => {
-        console.error(err);
+        loading.dismiss();
+          let alert = this.alertCtrl.create({
+            title: 'Error!',
+            subTitle: 'เกิดข้อผิดพลาด, ' + JSON.stringify(err),
+            buttons: ['OK']
+          });
+          alert.present();
       });
   }
 
